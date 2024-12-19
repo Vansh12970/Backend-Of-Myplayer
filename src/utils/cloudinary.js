e9081a258acdc26d3e69a66f1704758284694693
VANSH PRATAP SINGH <vanshpratapsingh1121@gmail.com>
2024-12-20 00:03:10 +0530

    file handling (cloudinary and multer)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+//cloudinaru is name of v2 now as wish
+//fs filesystem use to read, write, remove, path by default in node.js
+import fs from "fs"
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLODINARY_CLOUD_NAME, 
+    api_key: process.env.CLODINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload file on clodinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded sucessfully
+        console.log("file is uploaded on cloudinary",
+        response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        //remove the locally saves temporary fiel as upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
