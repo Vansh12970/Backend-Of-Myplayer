74c75a53980b99c3cf84aab23dd79a1446d65fba
VANSH PRATAP SINGH <vanshpratapsingh1121@gmail.com>
2024-12-20 02:17:14 +0530

    done

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,7 +1,7 @@
 class ApiError extends Error {
     constructor(
          statusCode,
-         message= "SomethingWent Wrong",
+         message= "Something Went Wrong",
          errors = [],
          stack = ""
     ){
